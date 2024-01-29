63382ef1f7fec9f892ce8514e28cd784dddded50
ducksquaddd <ducksquadddd@gmail.com>
2024-01-28 16:59:26 -0800

    global commands

--- src/handler/index.js
+++ src/handler/index.js
@@ -33,12 +33,12 @@ module.exports = async (client) => {
     });
     client.on("ready", async () => {
       // Register for a single guild
-      await client.guilds.cache
-        .get("1049783263956324462")
-        .commands.set(arrayOfSlashCommands);
+      // await client.guilds.cache
+      //   .get("1049783263956324462")
+      //   .commands.set(arrayOfSlashCommands);
       // console.log(client.user.username);
       // Register for all the guilds the bot is in
-      // await client.application.commands.set(arrayOfSlashCommands);
+      await client.application.commands.set(arrayOfSlashCommands);
     });
   });
 };
